d0534e1b487d0a07e3b2ab72ac1be0912f351025
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-28 10:12:41 +0300

    scan file

--- lib/services/scan_image_file.dart
+++ lib/services/scan_image_file.dart
@@ -1,19 +1,29 @@
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 
 import '../utils/filter_utils.dart';
 
-// Function to extract text from an image file using Google ML Vision
+/// Function to extract text from an image file using Google ML Vision.
 Future<String?> scanImageFile({
   required File imageFile, // Required input: image file
   required TextRecognizer textRecognizer, // Required input: text recognizer
 }) async {
-  InputImage inputImage = InputImage.fromFile(imageFile);
+  try {
+    // Convert the image file to an InputImage
+    InputImage inputImage = InputImage.fromFile(imageFile);
 
-  final RecognizedText recognizedText =
-      await textRecognizer.processImage(inputImage);
+    // Process the image to extract text using the textRecognizer
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
 
-  String? extractedText = filter(recognizedText);
+    // Filter the extracted text if needed
+    String? extractedText = filter(recognizedText);
 
-  return extractedText; // Return the extracted text
+    return extractedText; // Return the extracted text
+  } catch (e) {
+    // Handle any exceptions that occur during image processing
+    debugPrint('Error scanning image: $e');
+    return null; // Return null to indicate failure
+  }
 }
