e054affeb01f82f141bf4e89f1328cb63d3aa85c
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-28 10:11:25 +0300

    recharge

--- lib/services/recharge_code.dart
+++ lib/services/recharge_code.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:safaricom_airtime_scanner/services/send_ussd.dart';
 
+/// Function to initiate a recharge using a USSD code and display the result in a dialog.
 Future<void> rechargeCode(String code, BuildContext context) async {
-  await sendUssd(code).then(
-    (value) {
-      if (value != null) {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Recharge'),
-              content: Text(value),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Close'),
-                ),
-              ],
-            );
-          },
-        );
-      }
-    },
-  );
+  try {
+    await sendUssd(code).then(
+      (value) {
+        if (value != null) {
+          // If a response is received, show it in an AlertDialog
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Recharge'),
+                content: Text(value),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the dialog
+                    },
+                    child: const Text('Close'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      },
+    );
+  } catch (e) {
+    // Handle any exceptions that occur during the sendUssd operation
+    debugPrint('Error initiating recharge: $e');
+    // Optionally, show an error dialog to the user
+  }
 }
