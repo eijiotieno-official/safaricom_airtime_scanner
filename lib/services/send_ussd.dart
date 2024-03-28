36d5d0684299cbd217e5d05bf7305456dd0f0f00
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-28 10:13:31 +0300

    send ussd

--- lib/services/send_ussd.dart
+++ lib/services/send_ussd.dart
@@ -1,15 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:safaricom_airtime_scanner/data/database/service_providers_database.dart';
 import 'package:ussd_advanced/ussd_advanced.dart';
 
 import 'grant_permissions.dart';
 
+/// Function to send a USSD code and return the response.
 Future<String?> sendUssd(String code) async {
   try {
+    // Grant permissions required for sending USSD codes
     await grantPermissions();
+
+    // Construct refined code by adding prefix, suffix, and removing whitespace
     String refinedCode = "${ServiceProvidersDatabase.safaricom.prefix}$code#"
         .replaceAll(" ", "");
+
+    // Send the advanced USSD code and wait for the response
     return await UssdAdvanced.sendAdvancedUssd(code: refinedCode);
   } catch (e) {
-    return null;
+    // Handle any exceptions that occur during USSD sending
+    debugPrint('Error sending USSD: $e');
+    return null; // Return null to indicate failure
   }
 }
