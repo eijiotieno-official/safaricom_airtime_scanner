9e48c94c199cd03fbea798e44c36b9e6ea87921d
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-28 10:06:25 +0300

    copy

--- lib/services/copy_code.dart
+++ lib/services/copy_code.dart
@@ -2,17 +2,24 @@ import 'dart:async'; // Import the async library for asynchronous operations
 import 'package:clipboard/clipboard.dart';
 import 'package:safaricom_airtime_scanner/data/database/service_providers_database.dart'; // Import the clipboard package
 
-// Function to copy the provided code to the clipboard
+/// Function to copy the provided code to the clipboard after adding a prefix and suffix.
 Future<void> copyCode(String code) async {
   // Check if the code is not empty
   if (code.isNotEmpty) {
-    // Use the clipboard package to copy the code to the clipboard
-    String refinedCode = "${ServiceProvidersDatabase.safaricom.prefix}$code#"
-        .replaceAll(" ", "");
-    await FlutterClipboard.copy(
-        refinedCode); // Asynchronous operation to copy code
+    try {
+      // Construct refined code by adding prefix and suffix and removing whitespace
+      String refinedCode =
+          "${ServiceProvidersDatabase.safaricom.prefix}${code.trim()}#";
+      // Use the clipboard package to copy the code to the clipboard
+      await FlutterClipboard.copy(
+          refinedCode); // Asynchronous operation to copy code
+    } catch (e) {
+      // Handle any errors thrown during clipboard copying
+      print('Error while copying code: $e');
+      throw 'Failed to copy code to clipboard';
+    }
   } else {
     // Handle case where the code is empty by throwing an error
-    throw 'code is empty';
+    throw 'Code is empty';
   }
 }
